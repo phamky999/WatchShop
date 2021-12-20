7146e29b5a62840db7bed614fa95993cdccdc5c0
phamky999 <kypv99@gmail.com>
2021-12-20 19:51:19 +0700

    add gh-page, change readme file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ReactJS Project
-## Description: Building a simple UI for a watch store using ReactJS
-
+## Description: Building a simple UI for a watch store using ReactJS.
+## Link demo: https://phamky999.github.io/WatchShop/
+### Clone from: https://hublet-store.myshopify.com/
+### Tech: html, css, js (library Reactjs - hook basic), React Router DOM, Redux toolkit
--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import "./assets/css/bootstrap-grid.css";
 import "./assets/css/base.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
